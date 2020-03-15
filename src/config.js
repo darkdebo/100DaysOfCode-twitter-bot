94edc05648ffe7be4c9c7925df175e8e4a11bc19
Debojyoti Chakraborty <50295688+darkdebo@users.noreply.github.com>
2020-03-15 08:56:05 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-    consumer_key: '',  
-    consumer_secret: '',
-    access_token: '',  
-    access_token_secret: ''
+    consumer_key: '7pCncjjmdjTHdyUHQKouXfzqR',  
+    consumer_secret: 'hk8xHzrIrrcJfAZoef9XjqavWOQLnLuJsKaQdzjdNdREbbRkO0',
+    access_token: '876635674883084289-EyXSSAUP3cGtIKQGBw8IGHePSzNPyRd',  
+    access_token_secret: 'u834K3M1fLMlZr9utqLTH4fChgsF7Ez0nDzpLqs7UtSXf'
   }
